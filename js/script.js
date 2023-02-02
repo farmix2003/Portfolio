8d8fb5859f70622b888e510ba74e35bd4c8a9c99
farmix2003 <ffarrux386@gmail.com>
2023-02-02 21:58:17 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// 'use strict'
+
+// const changeBtn = document.querySelectorAll('a')
+
+// let selectedItem
+// changeBtn.forEach(btn =>{
+//     btn.addEventListener('click', e =>{
+//         document.querySelector('.active').classList.remove('active')
+//         btn.classList.add('active')
+//         console.log(btn);
+//         selectedItem = btn.id
+//     })
+// })
